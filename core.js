9c1c50540ac246a7eae499d7570d3976d8e9ad60
hzFishy <84443782+hzFishy@users.noreply.github.com>
2025-01-18 01:30:52 +0100

    big update

--- core.js
+++ core.js
@@ -1,13 +1,56 @@
 ﻿
 
-Base();
 
-function Base()
-{
-    
-}
 
 function EndFile()
 {
-    w3.includeHTML();
+    $.ajax(
+{
+        url: '/header.html',
+        method: 'get',
+        success: function(data)
+        {
+            $('#header').html(data);
+            
+            var parts = window.location.href.split('/');
+            var lastSegment = parts.pop() || parts.pop();
+            //console.log(lastSegment);
+
+            // wait for DOM be loaded
+            setTimeout(function ()
+            {
+                var reference = document.getElementById('hz-nav');
+                if (reference)
+                {
+                    var lastSegmentElement = document.getElementById(lastSegment);
+                    if (lastSegmentElement)
+                    {
+                        lastSegmentElement.classList.add('hz-active');
+                    }
+                }
+                else
+                {
+                    console.error('Element with ID "Timeline" not found.');
+                }
+            }, 0);
+        }
+    });
+}
+
+
+function OnMobileNavClick()
+{
+    var listElement = document.getElementById("hz-navbar-mobile-list");
+    var menuElement = document.getElementById("hz-navbar-mobile-menu");
+    if (listElement.classList.contains("hz-active"))
+    {
+        listElement.classList.remove("hz-active");
+        menuElement.classList.remove("hz-active");
+        
+    }
+    else
+    {
+        listElement.classList.add("hz-active");
+        menuElement.classList.add("hz-active");
+    }
 }
